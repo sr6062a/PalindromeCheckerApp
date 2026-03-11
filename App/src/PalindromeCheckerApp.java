d3b910b42f80d4e3fe9322131e9d85d9e2217424
sr6062a <rai05111999@gmail.com>
2026-03-11 21:47:55 +0530

    UC8

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,30 @@
+import java.util.LinkedList;
+
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String input = "level";
+
+        LinkedList<Character> list = new LinkedList<>();
+
+        for (char c : input.toCharArray()) {
+            list.add(c);
+        }
+
+        boolean isPalindrome = true;
+
+        while (list.size() > 1) {
+            char first = list.removeFirst();
+            char last = list.removeLast();
+
+            if (first != last) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+}
